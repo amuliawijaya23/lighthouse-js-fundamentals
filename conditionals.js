27f4e05058a3d3971eea2926d602d77562ab3f1a
amuliawijaya23 <amuliawijaya@gmail.com>
2022-02-14 00:01:08 -0800

    More javascript conditional practice example

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-const temperature = -10;
+const temperature = -50;
 
 if (temperature < 0) {
   console.log("Make sure you wear a scarf!");
@@ -9,3 +9,15 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!")
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+

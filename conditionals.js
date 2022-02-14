d2f75d4e9867a3fed75f6b5996d53e2c0c76eac7
amuliawijaya23 <amuliawijaya@gmail.com>
2022-02-13 23:31:21 -0800

    Javascript conditionals practice

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+const temperature = -10;
+
+if (temperature < 0) {
+  console.log("Make sure you wear a scarf!");
+} else if (temperature < 15) {
+  console.log("Wear something thick!");
+} else {
+  console.log("Take out your summer clothings!");
+}
+
+console.log("Now you're ready to go outside!")

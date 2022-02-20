ec3c857a0ff37680e16d3cd06b2320c2a94b200e
amuliawijaya23 <amuliawijaya@gmail.com>
2022-02-19 22:48:50 -0800

    using for loop on array

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+let list = [];
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  list.push(packingList[i]);
+}
+console.log(list);
